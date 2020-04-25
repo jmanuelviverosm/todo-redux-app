6766a9eb084be857a4e94f130972a146a0e090ab
Juan Manuel Viveros M <jviveros@existaya.com>
2020-04-25 11:44:56 -0500

    Primer commit

---
+++ src/app/todos/todo.reducer.ts
@@ -0,0 +1,50 @@
+import { createReducer, on } from '@ngrx/store';
+import { crear, toggle, editar, borrar, toggleAll, limpiarTodos } from './todo.actions';
+import { Todo } from './models/todo.model';
+
+export const estadoInicial: Todo[] = [
+  new Todo('Salvar el mundo'),
+  new Todo('Vencer a thanos'),
+  new Todo('Comprar yraje de Iroman'),
+  new Todo('Robar escudo del capitán america'),
+];
+
+const _todoReducer = createReducer(estadoInicial,
+  on(crear, (state, { texto }) => [...state, new Todo(texto)]),
+  on(limpiarTodos, state => state.filter(todo => !todo.completado)),
+  on(borrar, (state, { id }) => state.filter(todo => todo.id !== id)),
+  on(toggleAll, (state, { completado }) => state.map(todo => {
+    return {
+      ...todo,
+      completado: completado
+    };
+  })),
+  on(toggle, (state, { id }) => {
+    return state.map(todo => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          completado: !todo.completado
+        };
+      } else {
+        return todo;
+      }
+    });
+  }),
+  on(editar, (state, { id, texto }) => {
+    return state.map(todo => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          texto: texto
+        };
+      } else {
+        return todo;
+      }
+    });
+  })
+);
+
+export function todoReducer(state, action) {
+  return _todoReducer(state, action);
+}
